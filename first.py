222d90b977fe906cf3401fffbe0708b1552cc885
Enrico Emiro <16721212+enricoemiro@users.noreply.github.com>
2022-01-17 00:25:46 +0100

    Updated first() and add_blur_and_noise()
    Replaced sigma and kernlen with K

--- first.py
+++ first.py
@@ -3,9 +3,7 @@ import numpy as np
 from utils import *
 
 # Add blur and noise to the given image
-def add_blur_and_noise(image: np.array, sigma, kernlen, standard_deviation):
-  K = psf_fft(gaussian_kernel(kernlen, sigma), kernlen, image.shape)
-
+def add_blur_and_noise(image: np.array, K, standard_deviation):
   noise = np.random.normal(size=image.shape) * standard_deviation
 
   return A(image, K) + noise
@@ -17,8 +15,8 @@ def add_blur_and_noise(image: np.array, sigma, kernlen, standard_deviation):
 #   - σ = 1     dimension  7×7
 #   - σ = 1.3   dimension  9×9
 # and adding Gaussian noise with standard deviation (0.0, 0.05]
-def first(image: np.array, sigma, kernlen, standard_deviation):
-  corrupted_image = add_blur_and_noise(image, sigma, kernlen, standard_deviation)
+def first(image: np.array, K, standard_deviation):
+  corrupted_image = add_blur_and_noise(image, K, standard_deviation)
   metrics = generate_metrics(image, corrupted_image)
 
   return corrupted_image, metrics
