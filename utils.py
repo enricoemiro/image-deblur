bfb909766f512c12305df46b06d3868ad1a94a2f
Enrico Emiro <16721212+enricoemiro@users.noreply.github.com>
2022-01-17 15:18:55 +0100

    Fixed indentation of gaussian_kernel() and psf_fft
    Co-authored-by: Giulio Billi <72358159+giuliobilli01@users.noreply.github.com>

--- utils.py
+++ utils.py
@@ -5,28 +5,28 @@ from skimage import metrics
 
 # Create a Gaussian kernel of size kernlen and standard deviation sigma
 def gaussian_kernel(kernlen, sigma):
-    x = np.linspace(- (kernlen // 2), kernlen // 2, kernlen)
-    # Unidimensional Gaussian kernel
-    kern1d = np.exp(- 0.5 * (x**2 / sigma))
-    # Bidimensional Gaussian kernel
-    kern2d = np.outer(kern1d, kern1d)
-    # Normalization
-    return kern2d / kern2d.sum()
+  x = np.linspace(- (kernlen // 2), kernlen // 2, kernlen)
+  # Unidimensional Gaussian kernel
+  kern1d = np.exp(- 0.5 * (x**2 / sigma))
+  # Bidimensional Gaussian kernel
+  kern2d = np.outer(kern1d, kern1d)
+  # Normalization
+  return kern2d / kern2d.sum()
 
 # Compute the FFT of the kernel 'K' of size 'd' padding with the zeros necessary
 # to match the size of 'shape'
 def psf_fft(K, d, shape):
-    # Zero padding
-    K_p = np.zeros(shape)
-    K_p[:d, :d] = K
+  # Zero padding
+  K_p = np.zeros(shape)
+  K_p[:d, :d] = K
 
-    # Shift
-    p = d // 2
-    K_pr = np.roll(np.roll(K_p, -p, 0), -p, 1)
+  # Shift
+  p = d // 2
+  K_pr = np.roll(np.roll(K_p, -p, 0), -p, 1)
 
-    # Compute FFT
-    K_otf = fft.fft2(K_pr)
-    return K_otf
+  # Compute FFT
+  K_otf = fft.fft2(K_pr)
+  return K_otf
 
 # Multiplication by A
 def A(x, K):
